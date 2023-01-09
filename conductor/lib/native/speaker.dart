04d543728a71664160c501e899cf6b5c80fdfc7a
Rohan Mathew <robozmat@gmail.com>
2023-01-09 21:06:55 +0000

    wss

--- conductor/lib/native/speaker.dart
+++ conductor/lib/native/speaker.dart
@@ -1,31 +1,36 @@
+import 'dart:async';
+
 import 'package:conductor/native/bridge_generated.dart';
 import 'package:conductor/native/native.dart';
+import 'package:conductor/util/stream.dart';
 
 // wrapper around raw ffi
 class Speaker {
   RwLockRawSpeaker _x;
   String _ip;
-  String get ip => _ip;
+  get ip => _ip;
 
-  Speaker._(this._x, this._ip);
+  Speaker(this._ip) : this._x = api.speakerNew(ip: _ip);
 
-  static Future<Speaker> create(String ip) async {
-    return Speaker._(await api.speakerNew(ip: ip), ip);
+  void connect() {
+    api.speakerConnect(x: _x).listen(_onUpdate);
   }
 
-  Future<void> connect() async {
-    api.speakerConnect(x: _x);
-  }
+  bool isConnected() => api.speakerIsConnected(x: _x);
 
-  Future<bool> isConnected() async {
-    return api.speakerIsConnected(x: _x);
-  }
+  Future<void> ping() async => await api.speakerPing(x: _x);
 
-  Future<MachineInfo?> getInfo() async {
-    return api.speakerGetInfo(x: _x);
-  }
+  // update callbacks
+  Streamer<MachineInfo> _infos = Streamer();
+  Streamer<MachineInfo> get infos => _infos;
+  Streamer<int> _pings = Streamer();
+  Streamer<int> get pings => _pings;
 
-  Future<double> ping() async {
-    return api.speakerPing(x: _x);
+  void _onUpdate(Update u) {
+    if (u.field0.runtimeType == MachineInfo) {
+      _infos.next(u.field0 as MachineInfo);
+    } else if (u.field0.runtimeType == Ping) {
+      _pings.next((u.field0 as Ping).field0);
+    }
   }
 }
